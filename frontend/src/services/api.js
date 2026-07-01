27633493f035d66526b865b2c61d1e7d9e9939fc
AsifSamji <asifsamji0104@gmail.com>
2026-07-01 20:45:27 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smart-grocery-tracker-wg6t.onrender.com/api",
+  baseURL: "https://smart-grocery-tracker-eight.vercel.app/api",
 });
 
 API.interceptors.request.use((req) => {
